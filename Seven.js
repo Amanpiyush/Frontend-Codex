13f79fc7f5f92c7df4f1b60d3b4ce42aafc70f8a
Aman Piyush <amanpiyush.singh@gmail.com>
2026-03-03 09:05:32 +0530

    DOM

--- Seven.js
+++ Seven.js
@@ -1,9 +1,48 @@
-const Numbers = [1, 2, 3 ,4, 5, 6, 7, 8, 9, 10]
-
-
-const Num = Numbers.map( (Numn) => Numn + 10);
-console.log(Num)
-
-
 //Chaining 
 
+// const Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const userNum = Number.map( (Num)=>Num + 10)
+//                 .map( (Num)=> Num * 10)
+//                 .map( (Num)=> Num - 2)
+//                 .filter( (Num)=> Num > 140)
+
+                // console.log(userNum)
+
+
+
+//Reduce Methods
+// const Number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const userNumber = Number.reduce( (acc, curval) => {
+//     console.log(`acc ${acc}, curval ${curval}`)
+//     return acc + curval
+// }, 0)
+// const userNumber = Number.reduce( (acc, curval) => acc+curval, 0)
+// console.log(userNumber)
+
+
+
+const Shoppingcart = [
+    {
+        course : "JS",
+        price : 299
+    }, 
+    {
+        course : "C++",
+        price : 299
+    }, 
+    {
+        course : "HTML,CSS",
+        price : 5299
+    }, 
+    {
+        course : "Android Development",
+        price : 7299
+    }, 
+    {
+        course : "Data Science",
+        price : 78299
+    }, 
+]
+
+const user = Shoppingcart.reduce((acc, items) => acc+items.price, 0)
+console.log(user)
